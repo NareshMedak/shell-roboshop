89b0a2fd1537cc748fc2de3fc95cfded5070b9fd
naresh <medaknaresh66@gmail.com>
2026-02-18 11:54:53 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,21 @@
-#!/bin/bash/
+#!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-08a75b31e6d956d73"
-INSTANCE_ID=("mongodb" "catalogue" "mysql" "redis" "cart" "user" "payment" "shipping"
-"rabbitmq" "dispatch" "frontend")
+SG_ID="sg-08a75b31e6d956d73" 
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z09404561CJNYF83THUCK"
-DOMAIN_NAME="medaknaresh.digital"
+DOMAIN_NAME="medaknaresh,digital" replace with your domain
 
-for instance in ${INSTANCE[@]}
-do 
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08a75b31e6d956d73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].PrivateIpAddress" --output text)
-if [ $instance != "frontend"]
-then
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-   else
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text) 
-   fi
-   echo "$instance IP adress: $IP"
-   done
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08a75b31e6d956d73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
