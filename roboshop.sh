4dbcd563c4890b070269ed81682be19affc74a32
naresh <medaknaresh66@gmail.com>
2026-02-18 19:21:49 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-08a75b31e6d956d73" 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z09404561CJNYF83THUCK" 
-DOMAIN_NAME="medaknaresh.digital" 
+DOMAIN_NAME="medaknaresh.digital"
 
 for instance in ${INSTANCES[@]}
 do
