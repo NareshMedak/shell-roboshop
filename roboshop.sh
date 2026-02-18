ac4641fe31105ca60a2f83b69a30d033da34a44e
naresh <medaknaresh66@gmail.com>
2026-02-18 11:47:54 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,8 @@ DOMAIN_NAME="medaknaresh.digital"
 
 for instance in ${INSTANCE[@]}
 do 
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-08a75b31e6d956d73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].PrivateIpAddress" --output text)
-if [ $INSTANCE != "frontend"]
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08a75b31e6d956d73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].PrivateIpAddress" --output text)
+if [ $instance != "frontend"]
 then
    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
    else
