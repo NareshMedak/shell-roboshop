4b23e6136fd4de20e227ecfed146840970d403dd
naresh <medaknaresh66@gmail.com>
2026-02-18 11:58:27 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -4,19 +4,18 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-08a75b31e6d956d73" 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z09404561CJNYF83THUCK"
-DOMAIN_NAME="medaknaresh,digital" 
+DOMAIN_NAME="medaknaresh.digital" 
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08a75b31e6d956d73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+        
     fi
     echo "$instance IP address: $IP"
     done
