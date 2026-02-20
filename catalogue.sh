b5b93c188980ed40e0092304ee8d4da130bfa08c
naresh <medaknaresh66@gmail.com>
2026-02-20 09:01:04 +0530

    hello world

--- catalogue.sh
+++ catalogue.sh
@@ -81,6 +81,12 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
+STATUS=$(mongosh --host mongodb.medaknaresh.digital --val 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
 mongosh --host mongodb.medaknaresh.digital </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
 
