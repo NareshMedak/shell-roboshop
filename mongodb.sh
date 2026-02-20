5e9d60d13d8873ff2cc1e8233fc8078e20b7d6ee
naresh <medaknaresh66@gmail.com>
2026-02-20 07:41:54 +0530

    hello world

--- mongodb.sh
+++ mongodb.sh
@@ -49,7 +49,7 @@ VALIDATE $? "starting MongoDB"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing MongoDB conf file for remote connections"
 
-systemctl restart mongod &>>LOG_FILE
+systemctl restart mongod 
 VALIDATE $? "Restarting MongoDB"
 
 
