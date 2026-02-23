6c5407d334a89dbef72d10ab39b4217a5149f368
naresh <medaknaresh66@gmail.com>
2026-02-23 15:10:54 +0530

    hello world

--- catalogue.sh
+++ catalogue.sh
@@ -45,8 +45,7 @@ VALIDATE(){
    dnf install nodejs -y &>>$LOG_FILE
    VALIDATE $? "Installing nodejs:20"
 
-   id roboshop
-
+ id roboshop
  if [ $? - ne 0 ]
  then
    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
