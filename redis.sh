0d848263028e9ca709de71a5fb4a7f329cc8edd5
naresh <medaknaresh66@gmail.com>
2026-02-25 10:13:59 +0530

    hello world

--- redis.sh
+++ redis.sh
@@ -33,22 +33,22 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>>LOG_FILE
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling the redis"
 
-dnf module enable redis:7 -y &>>LOG_FILE
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling the redis"
 
-dnf install redis -y &>>LOG_FILE
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing the redis"
 
 sed -i -e 's/127.0.0.0/0.0.0.0/g' -e '/protected mode/ c protected mode no'/ /etc/redis/redis.conf
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis &>>LOG_FILE
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling Redis"
 
-systemctl start redis &>>LOG_FILE
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Started Redis"
 
 END_TIME=$(date +%s)
