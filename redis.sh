07ddf6f0bd22e084efee3808508bd3d0513ae933
naresh <medaknaresh66@gmail.com>
2026-02-25 11:26:06 +0530

    hello world

--- redis.sh
+++ redis.sh
@@ -49,7 +49,7 @@ systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling Redis"
 
 systemctl start redis  &>>$LOG_FILE
-VALIDATE $? "Started Redis"
+VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
