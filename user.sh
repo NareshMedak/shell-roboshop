88611f7df4e8278a41c87cde83f844c0284b9a6d
naresh <medaknaresh66@gmail.com>
2026-02-26 10:06:26 +0530

    hello world

--- user.sh
+++ user.sh
@@ -14,9 +14,7 @@ SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#check the priveleges or not 
-
-
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -29,54 +27,54 @@ fi
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e  "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e  "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
 
-   dnf module disable nodejs -y &>>$LOG_FILE
-   VALIDATE $? "Disbaling default nodejs" &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
 
-   dnf module enable nodejs:20 -y  &>>$LOG_FILE
-   VALIDATE $? "Enabling nodejs:20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
 
-   dnf install nodejs -y &>>$LOG_FILE
-   VALIDATE $? "Installing nodejs:20"
-
- id roboshop
- if [ $? -ne 0 ]
- then
-   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-   VALIDATE $? "Creatig roboshop system user"
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "system user roboshop already created...$Y $SKIPPING $N"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
-   mkdir -p /app 
-   VALIDATE $? "Creating app directory"
 
-   curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
-   VALIDATE $? "Downloadig the user"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading cart"
 
 rm -rf /app/*
 cd /app 
-unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping the user"
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "unzipping cart"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing the dependicies"
+VALIDATE $? "Installing Dependencies"
 
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
-VALIDATE $? "Copying user service"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copying cart service"
 
 systemctl daemon-reload &>>$LOG_FILE
-systemctl enable user &>>$LOG_FILE
-systemctl start user
-VALIDATE $? "Starting User"
+systemctl enable cart  &>>$LOG_FILE
+systemctl start cart
+VALIDATE $? "Starting cart"
 
-END_TIME=$(+%s)
-TOTAL_TIME=$(( $END_TIME-$START_TIME ) )
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo "Script exction completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
