4b2b45df2bfe345d9a7b659237e7e084cd9ca25d
naresh <medaknaresh66@gmail.com>
2026-03-03 17:50:50 +0530

    hello world

--- cart.sh
+++ cart.sh
@@ -75,6 +75,6 @@ systemctl start cart
 VALIDATE $? "Starting cart"
 
 END_TIME=$(date +%s)
-TOTOAL_TIME=$ (( $END_TIME -$START_TIME ))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" |  tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
